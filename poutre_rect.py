a62b515ddcdf92e3aff9b7ba7bac208c8299d416
LuuN2122 <lucyN2122@gmail.com>
2020-09-22 11:54:41 -0400

    fini

--- poutre_rect.py
+++ poutre_rect.py
@@ -7,10 +7,14 @@ b = 10  # en mm
 h = 20  # en mm
 
 # Calcul de l'inertie
-
-I = 
+I = (b*(h**3)) / 12
 
 # Calcul de la déformation maximale
+delta_max = (F*(L**3)) / (3*E*(10**3)*I)
 
-delta_max = 
+# Arrondir la valeur de la déformation maximale à deux décimales
+delta_max = str(round(delta_max, 2))
+
+print ("La déformation maximale de la poutre est", delta_max, "mm")
+
 
--- poutre_section.py
+++ poutre_section.py
@@ -1,3 +1,5 @@
+import math
+
 # Initialisation des variables
 F = 10000  # en N
 E = 210  # en GPa = 10^3 N/mm^2
@@ -17,6 +19,37 @@ d = 5  # en mm
 D = 15  # en mm
 d = 5  # en mm
 
+# Calcul de l'inertie de la section
+I_rectangle = (b * (h ** 3)) / 12
+I_carre = (a ** 4) / 12
+I_rond = (math.pi * (d ** 4)) / 64
+I_creux = (math.pi * ((D ** 4) - (d ** 4))) / 64
 
-# Calcul de la section optimale
+# Création d'une liste pour comparer les I
+liste_inertie = [I_rectangle, I_carre, I_rond, I_creux]
 
+# Chercher le max puisque plus le I est élevé, plus le delta est petit
+I = max(liste_inertie)
+
+# Calcul de la déformation maximale
+delta_max = (F * (L ** 3)) / (3 * E * (10 ** 3) * I)
+
+# Arrondir la valeur de la déformation maximale à deux décimales
+delta_max = str(round(delta_max, 2))
+
+# Choix de la section dépendamment du I choisi
+if I == I_rectangle:
+    section = "rectangulaire"
+
+elif I == I_carre:
+    section = "carrée"
+
+elif I == I_rond:
+    section = "ronde"
+
+elif I == I_creux:
+	section = "creuse"
+
+
+print ("Le type de section minimisant la déformation maximale est", section, ", avec une déformation de", delta_max,
+          "mm")
